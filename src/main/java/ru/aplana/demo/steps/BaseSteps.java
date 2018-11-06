52716999d280b06fc4446bb8cbe0a28597283154
Байкова Мария <Mariya.Baykova@aplana.com>
2018-11-07 02:24:43 +0300

    setting aws

--- src/main/java/ru/aplana/demo/steps/BaseSteps.java
+++ src/main/java/ru/aplana/demo/steps/BaseSteps.java
@@ -1,9 +1,11 @@
 package ru.aplana.demo.steps;
 
+
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -24,9 +26,10 @@ public class BaseSteps {
 	public void setUpNative() throws Exception {
 		//Определяем desired capabilities
 		DesiredCapabilities caps = new DesiredCapabilities();
-		caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0.1");
+//		caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0.1");
 		caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
-		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "YT9113N0NB");
+//		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "YT9113N0NB");
+		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
 		caps.setCapability("appPackage", "ru.rzd.pass");
 		caps.setCapability("appActivity", "ru.rzd.app.common.gui.SplashActivity");
 
@@ -35,18 +38,18 @@ public class BaseSteps {
 	}
 
 
-	@Before("@web")
-	public void setUpWeb() throws Exception {
-		//Определяем desired capabilities
-		DesiredCapabilities caps = new DesiredCapabilities();
-		caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0.1");
-		caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
-		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "YT9113N0NB");
-		caps.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-
+//	@Before("@web")
+//	public void setUpWeb() throws Exception {
+//		//Определяем desired capabilities
+//		DesiredCapabilities caps = new DesiredCapabilities();
+//		caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0.1");
+//		caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
+//		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "YT9113N0NB");
+//		caps.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+//		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//	}
+//
 
 	/* We disable the driver after EACH test has been executed. */
 	@After("@native")
@@ -54,8 +57,5 @@ public class BaseSteps {
 		driver.closeApp();
 	}
 
-	@After("@web")
-	public void tearDownWeb() {
-		driver.quit();
-	}
+
 }
